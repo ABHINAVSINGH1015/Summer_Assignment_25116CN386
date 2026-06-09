751e20753e6dfcb3cea5bc9aab69d170533e8149
ABHINAVSINGH1015 <25116cn386@.glbitm.ac.in>
2026-06-09 10:41:36 +0530

    DAY-9 is uploaded

---
+++ DAY-9/Question35.c
@@ -0,0 +1,23 @@
+//Write a program to Print repeated character pattern.
+//A 
+//BB 
+//CCC 
+//DDDD 
+//EEEEE 
+#include <stdio.h>
+int main() {
+    int i, j, rows;
+    char ch;
+    printf("Enter the number of rows: ");
+    scanf("%d", &rows);
+    
+    for(i = 1; i <= rows; ++i) {
+        ch = 'A' + i - 1; 
+        for(j = 1; j <= i; ++j) {
+            printf("%c ", ch);
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
